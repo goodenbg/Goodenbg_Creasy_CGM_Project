d5d63642724ebd03af768dd0b579d3c20eaa46f2
goodenbg <ggoodenbour@pm.me>
2026-07-01 10:44:13 -0600

    added helpful git/terminal commands

--- demoscript.R
+++ demoscript.R
@@ -1,8 +1,38 @@
 #This is a demo script to test our Git repository
 Print("Hello World")
 
+# creating new repository
+# 1. create local repository: make folder/ R project
+#   in terminal change directory to project location: cd <path>
+#   in terminal type ls to view files in directory
+# 2. configure git
+#   in terminal git conf --global user.name <username>
+#   in terminal git conf --global user.email <email>
+#     check set correctly git config -- get user.name or user.email
+# in terminal git init
+
 # cd - path to this directory
 # git status
  # git add ""
 # git commit -m "commit message"
 # git push -u origin master
+
+# in Git
+# Repositories -> new -> name 
+# helpful commands:
+# …or create a new repository on the command line
+
+# echo "# Goodenbg_Creasy_CGM_Project" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/goodenbg/Goodenbg_Creasy_CGM_Project.git
+# git push -u origin main
+
+# …or push an existing repository from the command line
+
+# 1. git remote add origin https://github.com/goodenbg/Goodenbg_Creasy_CGM_Project.git
+# git branch -M main
+# 2. git push -u origin main
+
