a949fcebec486e89643e8a3dd516f2a42e8fcd1e
goodenbg <ggoodenbour@pm.me>
2026-07-01 10:38:24 -0600

    added demoscript.R

---
+++ demoscript.R
@@ -0,0 +1,8 @@
+#This is a demo script to test our Git repository
+Print("Hello World")
+
+# cd - path to this directory
+# git status
+ # git add ""
+# git commit -m "commit message"
+# git push -u origin master
